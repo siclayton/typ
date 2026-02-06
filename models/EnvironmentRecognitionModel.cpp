08f7b73532d089e946494f896537c370e10bace9
Simon <clayto23@lancaster.ac.uk>
2026-02-06 16:53:12 +0000

    Added comments to Environment Recognition model

--- models/EnvironmentRecognitionModel.cpp
+++ models/EnvironmentRecognitionModel.cpp
@@ -11,10 +11,12 @@ int currentClass = 0; //The ID for the class that the user is currently providin
 int currentSample = 0; //The position in the samples array to add the next sample to
 int training = TRUE;
 
+// A sample to predict
 typedef struct {
     float features[FEATURE_COUNT];
 } EnvironmentSample;
 
+// A sample used to train the model (contains the class label)
 typedef struct {
     int sampleClass;
     EnvironmentSample sample;
@@ -57,6 +59,7 @@ NaiveBayesModel::NaiveBayesModel(int numFeatures, int numClasses, int lenXTrain,
     this->minFeatureValues = new float[numFeatures];
     this->maxFeatureValues = new float[numFeatures];
 
+    // Initialise the max and min values to the feature values from the first training sample
     for (int i = 0; i < numFeatures; i++) {
         float value = xTrain[0].sample.features[i];
         maxFeatureValues[i] = value;
@@ -84,6 +87,7 @@ void NaiveBayesModel::trainModel() {
     calcConditionalProbabilities();
 
     for (int i = 0; i < numClasses * numFeatures * NUM_BINS; i++) {
+        uBit.serial.printf("\r\n");
         uBit.serial.printf("%d, ", static_cast<int>(conditionalProbabilities[i] * 1000));
         if ((i + 1) % 4 == 0) uBit.serial.printf("\r\n");
     }
@@ -118,22 +122,26 @@ void NaiveBayesModel::calcConditionalProbabilities() {
 
                 //If the combination of class, feature and bin didn't appear in the dataset,
                 //set the probability to a very low number (so the combination isn't completely ignored during predictions)
-                if (conditionalProbabilities[index] == 0) conditionalProbabilities[index] = 1e-4;
+                if (conditionalProbabilities[index] == 0) conditionalProbabilities[index] = 1e-7;
             }
         }
     }
 }
 
+// Get the index of the array for the given class, feature and bin for the conditionalProbabilites array
+// This is needed as the conditionalProbabilties array is 1D
 int NaiveBayesModel::getCProbIndex(int c, int feature, int bin) {
     return c * numFeatures * NUM_BINS + feature * NUM_BINS + bin;
 }
 
+// Return the bin that the given value belongs in for the given feature
 int NaiveBayesModel::getBin(int feature, float value) {
     float range = maxFeatureValues[feature] - minFeatureValues[feature];
+
+    //If the range for that feature is too small, assign all values to bin 0
     if (range < 1e-6) return 0;
 
     float binSize = range / NUM_BINS;
-
     int bin = static_cast<int>((value - minFeatureValues[feature]) / binSize);
 
     //Ensure bin isn't out of range
@@ -148,7 +156,7 @@ int NaiveBayesModel::predict(EnvironmentSample sample) {
     float highestProbability = -1e30f; //Set highestProbability to a low negative number
 
     for (int i = 0; i < numClasses; i++) {
-        //Use log probabilities to prevent underflow
+        // Use log probabilities to prevent underflow
         float classProbability = log(classProbabilities[i]);
 
         for (int j = 0; j < numFeatures; j++) {
@@ -156,9 +164,11 @@ int NaiveBayesModel::predict(EnvironmentSample sample) {
             int bin = getBin(j, value);
             int cProbIndex = getCProbIndex(i, j, bin);
 
+            // Probabilities are calculated using Bayes theorem
             classProbability += log(conditionalProbabilities[cProbIndex]);
         }
 
+        // If this is the highest probability seen so far, update the prediction
         if (classProbability > highestProbability) {
             highestProbability = classProbability;
             prediction = i;
@@ -221,8 +231,8 @@ EnvironmentSample takeSample() {
 void onButtonA(MicroBitEvent e) {
     EnvironmentSample envSample = takeSample();
 
-    //If in training mode, label sample and add it to list of samples used to train the model
-    //Otherwise, predict the class of the sample collected
+    // If in training mode, label sample and add it to list of samples used to train the model
+    // Otherwise, predict the class of the sample collected
     if (training == TRUE) {
         TrainingSample sample = {currentClass, envSample};
         samples[currentSample++] = sample;
