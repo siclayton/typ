6a4b35e70b881a052b3df50a45ef12531bdac94c
Simon <clayto23@lancaster.ac.uk>
2026-02-21 12:47:34 +0000

    Implemented sample collection from the microphone

--- source/main.cpp
+++ source/main.cpp
@@ -2,21 +2,13 @@
 #include "arm_math.h"
 
 #define SAMPLE_RATE (11 * 1024)
-#define AUDIO_SAMPLES_NUMBER 1024
-#define FFT_SIZE 254
-#define NUM_FEATURES 10
+#define FFT_SIZE 512
+#define NUM_MEL 10
+#define NUM_FEATURES (NUM_MEL * 2)
 
+#define NUM_SAMPLES 150
 #define MAX_DEPTH 5
 #define MAX_NODES (((MAX_DEPTH + 1) * (MAX_DEPTH + 1)) - 1)
-
-MicroBit uBit;
-arm_rfft_fast_instance_f32 fftInstance;
-
-int currentClass = 0; //The ID for the class that the user is currently providing samples of
-int currentSample = 0; //The position in the samples array to add the next sample to
-bool training = true;
-int melBins[NUM_FEATURES + 2];
-float melWeights[NUM_FEATURES][FFT_SIZE / 2];
 
 typedef struct {
     int start, end; // Indexes of the indices array (in the DecisionTree class) that represent the part of the dataset passed to this node
@@ -39,16 +31,17 @@ typedef struct {
 
 class DecisionTree {
     public:
+        DecisionTree() = default;
         DecisionTree(int numFeatures, int numClasses, int lenXTrain, TrainingSample xTrain[]);
         int predict(SpeechSample sample);
     private:
-        int numFeatures;
-        int numClasses;
-        int lenXTrain;
-        int numNodes;
-        TrainingSample* xTrain;
-        TreeNode* nodes;
-        int* indices;
+        int numFeatures{};
+        int numClasses{};
+        int lenXTrain{};
+        int numNodes{};
+        TrainingSample* xTrain{};
+        TreeNode* nodes{};
+        int* indices{};
 
         void trainModel();
 };
@@ -117,46 +110,98 @@ void DecisionTree::trainModel() {
     }
 }
 
+// Global variables
+MicroBit uBit;
+arm_rfft_fast_instance_f32 fftInstance;
+DataSource& source = *uBit.audio.processor;
+DecisionTree model;
+
+int currentClass = 0; //The ID for the class that the user is currently providing samples of
+int currentSample = 0; //The position in the samples array to add the next sample to
+TrainingSample samples[NUM_SAMPLES];
+bool training = true;
+
+int melBins[NUM_MEL + 2];
+float melWeights[NUM_MEL][FFT_SIZE / 2];
+float window[FFT_SIZE];
+float fftOutput[FFT_SIZE];
+float magnitudeSpectrum[FFT_SIZE / 2];
+float melOutput[NUM_MEL];
+
 void applyMelFilters(float* fft, float* mel) {
     // Loop over the filters
-    for (int i = 0; i < NUM_FEATURES; i++) {
+    for (int i = 0; i < NUM_MEL; i++) {
         float sum = 0;
 
+        // Apply filter
         for (int j = melBins[i]; j < melBins[i + 2]; j++) {
             sum += fft[j] * melWeights[i][j];
         }
-
         mel[i] = logf(sum);
     }
 }
 
-SpeechSample takeSample() {
-
-    // Capture a seconds worth of audio
-        // Will output the buf used in fft function call
-
-    uBit.audio.mic->getSample();
-
+void applyfftAndMel() {
     // Calculate FFT and magnitude spectrum
-    auto* fftOutput = static_cast<float *>(malloc(sizeof(float) * AUDIO_SAMPLES_NUMBER));
-    auto* magnitudeSpectrum = static_cast<float *>(malloc(sizeof(float) * AUDIO_SAMPLES_NUMBER / 2));
-
-    arm_rfft_fast_f32(&fftInstance, buf + offset, fftOutput, 0);
-    arm_cmplx_mag_f32(fftOutput, magnitudeSpectrum, AUDIO_SAMPLES_NUMBER / 2);
+    arm_rfft_fast_f32(&fftInstance, window, fftOutput, 0);
+    arm_cmplx_mag_f32(fftOutput, magnitudeSpectrum, FFT_SIZE / 2);
 
     // Apply Mel Filterbank to the magnitude spectrum
-    auto* melOutput = static_cast<float *>(malloc(sizeof(float) * NUM_FEATURES));
-    applyMelFilters(fftOutput, melOutput);
+    applyMelFilters(magnitudeSpectrum, melOutput);
+}
+
+SpeechSample takeSample() {
+    int windowIndex = 0;
+    int count = 0;
+    uint64_t start = system_timer_current_time();
+
+    auto means = static_cast<float *>(calloc(NUM_MEL, sizeof(float)));
+    auto m2s = static_cast<float *>(calloc(NUM_MEL, sizeof(float)));
+
+    while (system_timer_current_time() - start < 1000) {
+        ManagedBuffer buf = source.pull();
+        uint8_t* bytes = buf.getBytes();
+
+        // Get the data back into 16 bit format
+        auto samples = reinterpret_cast<int16_t*>(bytes);
+        int numSamples = buf.length() / 2;
+
+        for (int i = 0; i < numSamples; i++) {
+            window[windowIndex++] = samples[i]  * (1.0f / 32768.0f); // Normalises the data to floats in the range (-1, 1)
+
+            if (windowIndex == FFT_SIZE) {
+                applyfftAndMel();
+                windowIndex = 0;
+                count++;
+
+                for (int j = 0; j < NUM_MEL; j++) {
+                    float diff = melOutput[j] - means[j];
+
+                    means[j] += diff / count;
+                    m2s[j] += diff * (melOutput[j] - means[j]);
+                }
+            }
+        }
+    }
+
+    count--;
+    auto variances = static_cast<float *>(calloc(NUM_MEL, sizeof(float)));
+    for (int i = 0; i < NUM_MEL; i++) {
+        variances[i] = m2s[i] / count;
+    }
 
     // Pack feature vector returned by mel filter bank calculations into a SpeechSample
     SpeechSample sample;
-    for (int i = 0; i < NUM_FEATURES; i++) {
-        sample.features[i] = melOutput[i];
+    for (int i = 0; i < NUM_MEL; i++) {
+        sample.features[i] = means[i];
+    }
+    for (int i = NUM_MEL; i < NUM_FEATURES; i++) {
+        sample.features[i] = variances[i];
     }
 
-    free(fftOutput);
-    free(magnitudeSpectrum);
-    free(melOutput);
+    free(means);
+    free(m2s);
+    free(variances);
 
     return sample;
 }
@@ -172,18 +217,18 @@ float melToHz(float mel) {
 void computeMelFilterbank() {
     float low = hzToMel(0);
     float high = hzToMel(SAMPLE_RATE / 2);
-    float step = (high - low) / NUM_FEATURES + 1;
+    float step = (high - low) / NUM_MEL + 1;
 
     // Calculate what mel values go into each fft bin
     // Need NUM_MEL_FILTERS + 2 as each of the triangles need 3 points (
-    for (int i = 0; i < NUM_FEATURES + 2; i++) {
+    for (int i = 0; i < NUM_MEL + 2; i++) {
         float hz = melToHz(low + i * step);
 
         melBins[i] = static_cast<int>((FFT_SIZE + 1) * hz / SAMPLE_RATE);
     }
 
     // Build mel triangle
-    for (int i = 0; i < NUM_FEATURES; i++) {
+    for (int i = 0; i < NUM_MEL; i++) {
         int left = melBins[i];
         int centre = melBins[i + 1];
         int right = melBins[i + 2];
@@ -200,7 +245,16 @@ void computeMelFilterbank() {
 }
 
 void onButtonA(MicroBitEvent e){
-    //Take sample
+    SpeechSample speechSample = takeSample();
+
+    if (training) {
+        TrainingSample sample = {currentClass, speechSample};
+        samples[currentSample++] = sample;
+    } else {
+        int prediction = model.predict(speechSample);
+        uBit.serial.printf("Prediction %d\r\n", prediction);
+        uBit.display.print(prediction);
+    }
 }
 void onButtonB(MicroBitEvent e);
 void onButtonAB(MicroBitEvent e);
@@ -210,9 +264,10 @@ int main() {
     uBit.audio.enable();
 
     // Initialise the fft instance
-    arm_rfft_fast_init_f32(&fftInstance, AUDIO_SAMPLES_NUMBER);
+    arm_rfft_fast_init_f32(&fftInstance, FFT_SIZE);
 
     // Compute Mel filterbank
+    computeMelFilterbank();
 
     uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, onButtonA);
     uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, onButtonB);
