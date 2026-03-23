3572e41b71660c3147ed8f89b77d4963bee10fdd
Simon <clayto23@lancaster.ac.uk>
2026-03-23 16:19:04 +0000

    Stopped weights attribute being allocated on the heap

--- models/VoiceActivityDetection/LogisticRegression.h
+++ models/VoiceActivityDetection/LogisticRegression.h
@@ -9,7 +9,6 @@
 
 class LogisticRegressionModel {
 public:
-    ~LogisticRegressionModel() {delete[] weights;} //A destructor that ensures the memory used for the weights isn't leaked
     LogisticRegressionModel(int numFeatures, int lenXTrain, TrainingSample xTrain[]);
     LogisticRegressionModel(int numFeatures, int lenXTrain, int maxIter, float lr, float threshold, TrainingSample[]);
     int predictClass(MicrophoneSample s);
@@ -21,7 +20,7 @@ private:
     float threshold{}; //The minimum amount the loss must decrease by to continue training
     TrainingSample* xTrain{};
     //Values to train using gradient descent
-    float* weights;
+    float weights[NUM_FEATURES];
     float bias;
     float epsilon; //Used to clamp predictions to ensure the log of 0 or 1 is never computed
 
